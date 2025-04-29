2e0dab6347f7c3f37763cc9ec46a66c586cb4385
Arif <arif@themascoteers.com>
2025-04-30 07:58:38 +1000

    done

--- algorithms/algorithm_v9.py
+++ algorithms/algorithm_v9.py
@@ -96,7 +96,7 @@ class Algorithm_v9(Algorithm):
             self.set_weights(mean_weight)
             mse_loss = self.criterion(y_hat, self.y_train)
             l1_loss = self.l1_loss(channel_weights)
-            lambda_value = self.get_lambda(epoch, l0_norm)
+            lambda_value = self.get_lambda(l0_norm)
             loss = mse_loss + lambda_value*l1_loss
             if epoch%10 == 0:
                 attn_handler.report_stats(self, channel_weights, sparse_weights, epoch, mse_loss, l1_loss.item(), lambda_value,loss)
